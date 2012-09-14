00b9eca72306961f7a058989c348bf8698e2cb77
johnjbarton <johnjbarton@johnjbarton.com>
2012-09-14 15:57:33 -0700

    begin patch stack

--- readme.txt
+++ readme.txt
@@ -7,5 +7,13 @@ Experimental extension of Chrome Devtools:
   Experimental patches to devtools adding new extension features.
 
 Branch syncToWebKit: a copy of Web Inspector source,
-  see tools/updateFromWebKit.sh
-  
+  see tools/updateFromWebKit.sh.
+  should only affect extension/WebInspectorKit
+
+Branch atopwi: a Web app/chrome extension shell around Web Inspector allowing it to run
+  over chrome.devtools.debugger extension protocol.
+  This branch sits at the lowest point we can test 
+
+Patch Branch: NNNNN_XYZ branches are patches on Web Inspector.
+  These patches should only change patches/ and extension/WebInspectorKit
+  These branches form a patch queue, see rebaseStack.sh
---
+++ tools/rebaseStack.sh
@@ -0,0 +1,23 @@
+#!/bin/sh
+# Copyright (c) 2010 Google Inc johnjbarton@google.com
+# Google BSD license http://code.google.com/google_bsd_license.html
+
+# Propagate changes in syncToWebKit up through patch stack chain
+# Use after updateFromWebKit.sh
+
+set -e -x
+git checkout syncToWebKit
+git checkout atopwi
+git checkout 93166_shortestItemSelected
+git checkout 96040_ExtensionItemSelector
+git checkout 96148_focusExtensionPanel
+#git checkout extendable
+#git checkout purple
+
+git rebase syncToWebKit atopwi
+git rebase atopwi 93166_shortestItemSelected
+git rebase 93166_shortestItemSelected 96040_ExtensionItemSelector
+git rebase 96040_ExtensionItemSelector 96148_focusExtensionPanel
+#git rebase DebuggerProtocol extendable
+#git rebase extendable purple
+#git rebase purple master
