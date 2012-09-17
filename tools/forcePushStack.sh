b97ee98605b28bcca77ad599f5e5186bb4f07414
johnjbarton <johnjbarton@johnjbarton.com>
2012-09-17 16:50:01 -0700

    add shell script to force push branches

---
+++ tools/forcePushStack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+# Copyright (c) 2010 Google Inc johnjbarton@google.com
+# Google BSD license http://code.google.com/google_bsd_license.html
+
+# Propagate changes in patch stack chain to origin
+# Use after  rebaseStack.sh
+
+set -e -x
+git push -f origin syncToWebKit
+git push -f origin atopwi
+git push -f origin 93166_shortestItemSelected
+git push -f origin 96040_ExtensionItemSelector
+git push -f origin 96148_focusExtensionPanel
