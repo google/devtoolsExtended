062a504f1cf0e0635861264d4de81f1366e8a2dc
johnjbarton <johnjbarton@johnjbarton.com>
2013-01-02 11:21:52 -0800

    update to stgit based workflow

--- tools/rebaseStack.sh
+++ tools/rebaseStack.sh
@@ -6,19 +6,7 @@
 # Use after updateFromWebKit.sh
 
 set -e -x
-git checkout syncToWebKit
-git checkout 97768_revert_implicit_star_beginning
-git checkout 97332_revert_open_resource_dialog_filter
-git checkout 93166_shortestItemSelected
-git checkout 96040_ExtensionItemSelector
-git checkout remoteDebug
-git checkout DebuggerProtocol
+git checkout stgit
+stg rebase syncToWebKit 
 git checkout atopwi
-
-git rebase syncToWebKit 97768_revert_implicit_star_beginning
-git rebase 97768_revert_implicit_star_beginning 97332_revert_open_resource_dialog_filter
-git rebase 97332_revert_open_resource_dialog_filter 93166_shortestItemSelected
-git rebase 93166_shortestItemSelected 96040_ExtensionItemSelector
-git rebase 96040_ExtensionItemSelector  remoteDebug 
-git rebase remoteDebug  DebuggerProtocol 
-git rebase DebuggerProtocol  atopwi 
+git rebase stgit
