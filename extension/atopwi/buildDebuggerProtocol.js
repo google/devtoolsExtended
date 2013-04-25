97cd5da682671a3722917a811eb3d039d8020dc2
johnjbarton <johnjbarton@johnjbarton.com>
2013-04-25 15:03:57 -0700

    Inspector.json was renamed to protocol.json

--- extension/atopwi/buildDebuggerProtocol.js
+++ extension/atopwi/buildDebuggerProtocol.js
@@ -2,12 +2,10 @@
 // Copyright 2013 Google Inc. johnjbarton@google.com
 
 (function buildDebuggerProtocol() {
-
   if (chrome && chrome.devtools) {
-    var protocolSrc = generateProxyDebugAPI.loadInspectorJSON("chrome-extension://ggimboaoffjaeoblofehalflljohnfbl/WebInspectorKit/Source/devtools/front_end/Inspector.json");
+    var protocolSrc = generateProxyDebugAPI.loadInspectorJSON("chrome-extension://ggimboaoffjaeoblofehalflljohnfbl/WebInspectorKit/Source/devtools/protocol.json");
     eval(protocolSrc);
   } else {
     console.error("buildDebuggerProtocol: no chrome.devtools in this scope; chrome:", chrome);
   }
-
 }());
