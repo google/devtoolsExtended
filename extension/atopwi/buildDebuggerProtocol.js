9d3b9d004ef3e82eff68d76bbb3f048804f9dcd5
johnjbarton <johnjbarton@johnjbarton.com>
2013-04-25 15:03:56 -0700

    generate chrome.devtools.protocol to call chrome.devtools.remoteDebug

---
+++ extension/atopwi/buildDebuggerProtocol.js
@@ -0,0 +1,13 @@
+// Google BSD license http://code.google.com/google_bsd_license.html
+// Copyright 2013 Google Inc. johnjbarton@google.com
+
+(function buildDebuggerProtocol() {
+
+  if (chrome && chrome.devtools) {
+    var protocolSrc = generateProxyDebugAPI.loadInspectorJSON("chrome-extension://ggimboaoffjaeoblofehalflljohnfbl/WebInspectorKit/Source/WebCore/inspector/front-end/Inspector.json");
+    eval(protocolSrc);
+  } else {
+    console.error("buildDebuggerProtocol: no chrome.devtools in this scope; chrome:", chrome);
+  }
+
+}());
